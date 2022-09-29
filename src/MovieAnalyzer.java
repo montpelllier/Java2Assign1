8c0776a039bbb0c781c8f0850bdd04984ee25f5d
montpelllier <1565975426@qq.com>
2022-09-29 15:15:09 +0800

    add gitignore and initialize methods in MovieAnalyzer.java

--- src/MovieAnalyzer.java
+++ src/MovieAnalyzer.java
@@ -1,4 +1,36 @@
+import java.util.List;
+import java.util.Map;
 
 public class MovieAnalyzer {
+    /**
+     * @param dataset_path
+     */
+    public MovieAnalyzer(String dataset_path) {
+
+    }
+
+    public Map<Integer, Integer> getMovieCountByYear() {
+        return null;
+    }
+
+    public Map<String, Integer> getMovieCountByGenre() {
+        return null;
+    }
+
+    public Map<List<String>, Integer> getCoStarCount() {
+        return null;
+    }
+
+    public List<String> getTopMovies(int top_k, String by) {
+        return null;
+    }
+
+    public List<String> getTopStars(int top_k, String by) {
+        return null;
+    }
+
+    public List<String> searchMovies(String genre, float min_rating, int max_runtime) {
+        return null;
+    }
 
 }
